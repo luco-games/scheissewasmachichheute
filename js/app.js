dffefed5725e7e19752e1d485ab646bc2a60a64b
h8h <mc2m382@chomeyer.de>
2026-01-14 23:06:10 +0100

    Two different levels and more design

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-let cachedLines = null; // wird nach dem ersten Laden gefüllt
-
-async function loadLinesOnce(url) {
+async function loadLinesOnce(cachedLines, url) {
   if (cachedLines !== null) {
     return cachedLines; // schon geladen → direkt zurückgeben
   }
@@ -21,76 +19,87 @@ function getCryptoRandomIndex(max) {
   return array[0] % max;
 }
 
-async function getRandomLine(url) {
-  const lines = await loadLinesOnce(url);
+async function getRandomLine(cache, url) {
+  const lines = await loadLinesOnce(cache, url);
   const index = getCryptoRandomIndex(lines.length);
   return lines[index];
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const url = "data/ideas.txt";
   const container = document.querySelector('.fire')
-    const fireworks = new Fireworks.default(container, {
-  autoresize: false,
-  opacity: 0.5,
-  acceleration: 1.05,
-  friction: 0.97,
-  gravity: 1.5,
-  particles: 50,
-  traceLength: 3,
-  traceSpeed: 10,
-  explosion: 5,
-  intensity: 30,
-  flickering: 50,
-  lineStyle: 'round',
-  hue: {
-    min: 0,
-    max: 360
-  },
-  delay: {
-    min: 0,
-    max: 60
-  },
-  rocketsPoint: {
-    min: 50,
-    max: 50
-  },
-  lineWidth: {
-    explosion: {
-      min: 1,
-      max: 3
+  const fireworks = new Fireworks.default(container, {
+    autoresize: false,
+    opacity: 0.5,
+    acceleration: 1.05,
+    friction: 0.97,
+    gravity: 1.5,
+    particles: 50,
+    traceLength: 3,
+    traceSpeed: 10,
+    explosion: 5,
+    intensity: 30,
+    flickering: 50,
+    lineStyle: 'round',
+    hue: {
+      min: 0,
+      max: 360
     },
-    trace: {
-      min: 1,
-      max: 2
+    delay: {
+      min: 0,
+      max: 60
+    },
+    rocketsPoint: {
+      min: 50,
+      max: 50
+    },
+    lineWidth: {
+      explosion: {
+        min: 1,
+        max: 3
+      },
+      trace: {
+        min: 1,
+        max: 2
+      }
+    },
+    brightness: {
+      min: 50,
+      max: 80
+    },
+    decay: {
+      min: 0.015,
+      max: 0.03
+    },
+    mouse: {
+      click: false,
+      move: false,
+      max: 1
     }
-  },
-  brightness: {
-    min: 50,
-    max: 80
-  },
-  decay: {
-    min: 0.015,
-    max: 0.03
-  },
-  mouse: {
-    click: false,
-    move: false,
-    max: 1
-  }
-});
+  });
   const shittyIdeaEl = document.querySelector(".shittyidea");
   const notThatEl = document.querySelector(".notthat");
-  const takeitEl = document.querySelector('.takeit')
+  const takeitEl = document.querySelector('.takeit');
+  const modusEl = document.querySelector('.modus');
+
+  let cachedLinesEasy = null; // wird nach dem ersten Laden gefüllt
+  let cachedLinesComplex = null; // wird nach dem ersten Laden gefüllt
+  let modus = "complex";
+  const urleasy = "data/ideas_easy.txt";
+  const urlcomplex = "data/ideas_complex.txt";
 
   async function updateLine() {
-    const line = await getRandomLine(url);
+    let line = "";
+    console.log(modus);
+    if (modus == "easy") {
+      line = await getRandomLine(cachedLinesEasy, urleasy);
+    } else {
+      line = await getRandomLine(cachedLinesComplex, urlcomplex);
+    }
+
     if (line && shittyIdeaEl) {
       shittyIdeaEl.textContent = line;
     }
   }
-
-
 
   // Initial laden + anzeigen
   updateLine();
@@ -107,5 +116,18 @@ document.addEventListener("DOMContentLoaded", async () => {
         fireworks.stop();
     });
     } );
+
+    if (modusEl) {
+      modusEl.addEventListener("click", async() => { 
+        if (modus == "easy") {
+          modus = "complex"
+          modusEl.textContent = "Gib mir was einfaches"
+        } else {
+          modus = "easy"
+          modusEl.textContent = "Ich schaff heut mehr!"
+        }
+        updateLine();
+      });
+    }
   }
 });
